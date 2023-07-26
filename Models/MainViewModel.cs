43d85b56c10342a93a728923d0c3f62371bc71f3
Malenci <lowskill1200@gmail.com>
2023-07-26 20:07:20 +0300

    add ViewModel.cs and MainViewModel.cs

---
+++ Models/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notepad.Models
+{
+    class MainViewModel : ViewModel
+    {
+        private string _input;
+        public string Input { get { return _input; } set {  _input = value; OnPropertyChanged(); } }
+    }
+}
