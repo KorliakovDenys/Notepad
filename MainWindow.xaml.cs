011d25cb320bb852c6c9f6e65e0f0dd008b1176b
Malenci <lowskill1200@gmail.com>
2023-08-24 18:12:29 +0300

    Row/Column interface

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.ComponentModel;
 using System.Windows;
+using System.Windows.Controls;
+using Notepad.Core;
 using Notepad.Models;
 
 namespace Notepad{
@@ -12,10 +14,22 @@ namespace Notepad{
         public MainWindow(){
             InitializeComponent();
             this.DataContext = _mainViewModel;
+           
         }
 
         private void MainWindow_OnClosing(object? sender, CancelEventArgs e){
             if (!_mainViewModel.ShowSaveFileCheck()) e.Cancel = true;
+        }
+
+        private void MainTexBox_OnSelectionChanged(object sender, RoutedEventArgs e)
+        {
+            if (sender is not TextBox textBox) return;
+
+            var selectionStart = textBox.SelectionStart;
+            var lineIndex = textBox.GetLineIndexFromCharacterIndex(selectionStart);
+
+            _mainViewModel.Start = lineIndex;
+            _mainViewModel.Length = textBox.CaretIndex - textBox.GetCharacterIndexFromLineIndex(lineIndex);
         }
     }
 }
